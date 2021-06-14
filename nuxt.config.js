810276c9615ecb96ac1cf6b114b1aa26883d16fd
Brandon Fan <bxfan@umich.edu>
2021-06-14 11:59:00 -0500

    add firebase through script tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,6 +7,20 @@ export default theme({
   head: {
     link: [
       { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/katex@0.11.0/dist/katex.min.css' }
+    ],
+    script: [
+      {
+        src: "/__/firebase/8.6.7/firebase-app.js",
+        body: true,
+      },
+      {
+        src: "/__/firebase/8.6.7/firebase-analytics.jss",
+        body: true,
+      },
+      {
+        src: "/__/firebase/init.js",
+        body: true,
+      },
     ]
   },
   content: {
