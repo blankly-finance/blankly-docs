d3f319649dec3d47d5a6884e04711288aaac5bb5
Brandon Fan <brandonfan1256@gmail.com>
2021-07-18 12:19:52 -0500

    update meta tags

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,42 @@ export default theme({
     primaryColor: '#79eea6'
   },
   head: {
+    title: "Build Quant Models with Blankly | Documentation",
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Blankly makes it extremely easy for you to build and deploy your models at scale, allowing you to write your code once and run it on any exchange. Check out our docs for more examples.'
+      },
+      {
+        hid: 'url',
+        name: 'url',
+        content: 'https://docs.blankly.finance',
+      },
+      {
+        hid: 'twitter:title',
+        name: 'twitter:title',
+        content: 'Build Quant Models with Blankly | Documentation',
+      },
+      {
+        hid: 'twitter:image',
+        name: 'twitter:image',
+        content: 'https://docs.blankly.finance/preview.png',
+      },
+      {
+        hid: 'twitter:description',
+        name: 'twitter:description',
+        content:
+          'Blankly makes it extremely easy for you to build and deploy your models at scale, allowing you to write your code once and run it on any exchange. Check out our docs for more examples.',
+      },
+      {
+        hid: 'twitter:card',
+        name: 'twitter:card',
+        content: 'summary_large_image',
+      },
+    ],
     link: [
       { rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/katex@0.11.0/dist/katex.min.css' }
     ],
